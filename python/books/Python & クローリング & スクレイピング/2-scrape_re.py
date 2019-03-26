73068f88f2a67e9b01b8a94cfc9543894fc43614
t_asai <t_asai@interspace.ne.jp>
2019-03-26 11:03:37 +0900

    2 章途中まで

---
+++ python/books/Python & クローリング & スクレイピング/2-scrape_re.py
@@ -0,0 +1,12 @@
+import re
+from html import unescape
+
+with open('dp.html') as f:
+    html = f.read()
+
+for partial_html in re.findall(r'<a itemprop="url".*?</ul>\s*</a></li>', html, re.DOTALL):
+    url = re.search(r'<a itemprop="url" href="(.*?)">', partial_html).group(1)
+    title = re.search(r'<p itemprop="name".*?</p>', partial_html).group(0)
+    title = re.sub(r'<.*?>', '', title)
+    title = unescape(title)
+    print(url, title)
---
+++ python/books/Python & クローリング & スクレイピング/2-venv.md
@@ -0,0 +1,13 @@
+# Python のインストールと実行
+
+Python 3 のデフォルト機能として venv がある
+pyvenv などがあったが、非推奨になりこちらを使うよう推奨されている
+
+```bash
+$ python3 -m venv scraping
+$ . scraping/bin/activate
+```
+
+```bash
+$ python 2-urlopen_encoding.py > dp.html
+```

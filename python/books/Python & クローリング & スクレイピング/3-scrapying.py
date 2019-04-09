c764e36f64770fce7da68110684ea9fccd3c176e
t_asai <t_asai@interspace.ne.jp>
2019-04-09 13:33:24 +0900

    Implement crawler

--- python/books/Python & クローリング & スクレイピング/3-scrapying.py
+++ python/books/Python & クローリング & スクレイピング/3-scrapying.py
@@ -2,24 +2,25 @@ import requests
 import bs4
 from urllib.parse import urljoin
 from functools import reduce
+import time
 
-url = "https://gihyo.jp/dp"
 
-
-def list_books(html):
+def list_books(html, base_url):
     soup = bs4.BeautifulSoup(html, "html.parser")
-    links = soup.find_all('a', {"itemprop": "url"})
-    hrefs = list(map(lambda x: urljoin(url, x.get("href")), links))
+    # links = soup.find_all('a', {"itemprop": "url"})
+    links = soup.select('#listBook a[itemprop="url"]')  # CSS セレクタの場合は select
+    hrefs = list(map(lambda x: urljoin(base_url, x.get("href")), links))
     return hrefs
 
 
-def get_book_detail(html):
+def get_book_detail(html, base_url):
     soup = bs4.BeautifulSoup(html, "html.parser")
     title = soup.find("h1", {"id": "bookTitle"}).get_text()
     price = soup.find("span", {"class": "buy"}).get_text()
     content = reduce(lambda s, x: s + x,
                      [y.get_text() for y in soup.find_all("h3")])
     book = {
+        "url": base_url,
         "title": title,
         "price": price,
         "content": content,
@@ -27,8 +28,25 @@ def get_book_detail(html):
     return book
 
 
+def main():
+    session = requests.Session()
+    base_url = "https://gihyo.jp/dp"
+    response = session.get(base_url)
+    urls = list_books(response.content, base_url)
+    for url in urls:
+        time.sleep(1)
+        resp = session.get(url)
+        book = get_book_detail(resp.content, url)
+        print(book)
+        print("\n")
+
+
+if __name__ == '__main__':
+    main()
+
+
 # TODO: db へ保存する
 # response = requests.get(url)
 # print(list_books(response.content))
-response = requests.get("https://gihyo.jp/dp/ebook/2019/978-4-297-10507-5")
-print(get_book_detail(response.content))
+# response = requests.get("https://gihyo.jp/dp/ebook/2019/978-4-297-10507-5")
+# print(get_book_detail(response.content))
